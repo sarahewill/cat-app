cad01bc4637d854c96a7812d0575213e34f6f37a
sarahwilliams <sarahewilliams@outlook.com>
2020-09-18 14:20:53 -0700

    add list of cats from catapi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const apiKey = 'c7edd4ac-90c9-41ed-9d2b-41bbace5e6e5';
+const url = 'https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc';
+
 function App() {
+    const [cats, setCats] = useState([]);
+
+    useEffect(() => {
+        getCats();
+    },[])
+    const getCats = () => {
+        fetch(url)
+            .then((res) => res.json())
+            .then(cats => {
+                setCats(cats);
+            })
+            .catch((error) => {
+                console.log('error', error);
+            })
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={getCats}>Get new cats</button>
+        <div>
+            {cats.map((cat) =>
+                <img src={cat.url} key={cat.id} alt="" />
+            )}
+        </div>
     </div>
   );
 }
